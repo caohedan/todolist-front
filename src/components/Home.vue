9cdb7c0b70dfdc12c312ac8aeb8700adf858ae44
曹鹤丹 <amelia_cao@caohedandeMacBook-Pro.local>
2019-03-10 23:53:25 +0800

    login page

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-header>Welcome To Todo List</el-header>
+    <el-main><Login/></el-main>
+  </el-container>
+</template>
+
+<script>
+    import Login from "./Login";
+    export default {
+        name: "Home",
+      components: {Login}
+    }
+</script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #303133;
+    font-size: large;
+
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 500px;
+  }
+
+</style>
